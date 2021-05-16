78748d8f4d49193bd44056157638cb5344e60df1
RuthFromTelAvivIsrael <3120958@gmail.com>
2021-05-16 03:16:48 +0300

    ל

--- BLL/WordInform.cs
+++ BLL/WordInform.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,38 +6,35 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    class WordInform
-    {
-        public WordInform()
-        {
-        }
+  public class WordInform
+  {
+    /// <summary>
+    /// אינדקס המילה בשיר
+    /// </summary>
+    public int SongIndex { get; set; }
+    /// <summary>
+    /// המילה
+    /// </summary>
+    public string Word { get; set; }
+    /// <summary>
+    /// אינדקס התגית שאליה המילה שיכת
+    /// </summary>
+    public int TagIndex { get; set; }
+    /// <summary>
+    /// מספר המילים בתגית אליה המילה שיכת
+    /// </summary>
+    public int TagCountWords { get; set; }
 
-        public WordInform(int songIndex, string word, int tagIndex, int tagCountWords)
-        {
-            SongIndex = songIndex;
-            Word = word;
-            TagIndex = tagIndex;
-            TagCountWords = tagCountWords;
-        }
+  public WordInform()
+  {
+  }
 
-
-
-
-        /// <summary>
-        /// אינדקס המילה בשיר
-        /// </summary>
-        public int SongIndex { get; set; }
-        /// <summary>
-        /// המילה
-        /// </summary>
-        public string Word { get; set; }
-        /// <summary>
-        /// אינדקס התגית שאליה המילה שיכת
-        /// </summary>
-        public int TagIndex { get; set; }
-        /// <summary>
-        /// מספר המילים בתגית אליה המילה שיכת
-        /// </summary>
-        public int TagCountWords { get; set; }
-    }
+  public WordInform(int songIndex, string word, int tagIndex, int tagCountWords)
+  {
+    SongIndex = songIndex;
+    Word = word;
+    TagIndex = tagIndex;
+    TagCountWords = tagCountWords;
+  }
+}
 }
